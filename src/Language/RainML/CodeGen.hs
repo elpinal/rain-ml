2f45592594616ce6181e9e7267e94cfbabdff177
elpinal <elpinal@users.noreply.github.com>
2018-12-02 14:49:23 +0900

    Generate a "move immediate" instruction

--- src/Language/RainML/CodeGen.hs
+++ src/Language/RainML/CodeGen.hs
@@ -1,11 +1,26 @@
+{-# LANGUAGE BinaryLiterals #-}
+
 module Language.RainML.CodeGen
   ( codeGen
   ) where
 
 import qualified Data.ByteString.Lazy as B
 import Data.ByteString.Builder
+import Data.Coerce
+import Data.Word
 
 import Language.RainML.Version
 
+newtype Reg = Reg Word8
+  deriving (Eq, Show)
+
 codeGen :: Int -> B.ByteString
-codeGen n = toLazyByteString $ word8 rainvmVersion <> word32BE (fromIntegral n)
+codeGen n = toLazyByteString $ mconcat
+  [ word8 rainvmVersion
+  , word8 0b100
+  , reg $ Reg 0
+  , word32BE $ fromIntegral n
+  ]
+
+reg :: Reg -> Builder
+reg = word8 . coerce
