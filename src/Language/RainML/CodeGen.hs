ef7d86d9c8b9928d9c36d46b4425ade511eb30fd
elpinal <elpinal@users.noreply.github.com>
2018-12-11 17:33:12 +0900

    Generate a "halt" instruction at the end of an output

--- src/Language/RainML/CodeGen.hs
+++ src/Language/RainML/CodeGen.hs
@@ -4,6 +4,7 @@ module Language.RainML.CodeGen
   ( codeGen
   ) where
 
+import Data.Bits
 import qualified Data.ByteString.Lazy as B
 import Data.ByteString.Builder
 import Data.Coerce
@@ -14,13 +15,20 @@ import Language.RainML.Version
 newtype Reg = Reg Word8
   deriving (Eq, Show)
 
+newtype Opcode = Opcode Word8
+  deriving (Eq, Show)
+
 moveImm :: Word8
 moveImm = 0b100
+
+opcodeHalt :: Opcode
+opcodeHalt = Opcode 1
 
 codeGen :: Int -> B.ByteString
 codeGen n = toLazyByteString $ mconcat
   [ word8 rainvmVersion
   , moveImmediate (Reg 0) n
+  , halt
   ]
 
 moveImmediate :: Reg -> Int -> Builder
@@ -32,3 +40,9 @@ moveImmediate dest imm = mconcat
 
 reg :: Reg -> Builder
 reg = word8 . coerce
+
+halt :: Builder
+halt = opcode opcodeHalt
+
+opcode :: Opcode -> Builder
+opcode = word8 . (`shiftL` 3) . coerce
