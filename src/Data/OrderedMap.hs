db5f6f5a0f712ca65c922db87f51f7f3ccdefefd
elpinal <elpinal@users.noreply.github.com>
2019-01-27 20:08:32 +0900

    Add Data.OrderedMap

---
+++ src/Data/OrderedMap.hs
@@ -0,0 +1,17 @@
+module Data.OrderedMap
+  ( OrderedMap
+  , empty
+  , insert
+  ) where
+
+import qualified Data.Map.Lazy as Map
+
+data OrderedMap k a = OrderedMap [k] (Map.Map k a)
+
+empty :: OrderedMap k a
+empty = OrderedMap [] Map.empty
+
+insert :: Ord k => k -> a -> OrderedMap k a -> Maybe (OrderedMap k a)
+insert k v (OrderedMap ks m)
+  | Map.member k m = Nothing
+  | otherwise      = Just $ OrderedMap (k : ks) $ Map.insert k v m
