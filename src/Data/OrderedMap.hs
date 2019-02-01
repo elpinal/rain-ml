2f75fb859b4101aaf1398da89c12a005b4087fc3
elpinal <elpinal@users.noreply.github.com>
2019-02-01 20:26:18 +0900

    Enrich OrderedMap

--- src/Data/OrderedMap.hs
+++ src/Data/OrderedMap.hs
@@ -1,13 +1,18 @@
+{-# LANGUAGE DeriveFunctor #-}
+
 module Data.OrderedMap
   ( OrderedMap
   , empty
   , insert
+
+  -- * Conversion
+  , toUnorderedMap
   ) where
 
 import qualified Data.Map.Lazy as Map
 
 data OrderedMap k a = OrderedMap [k] (Map.Map k a)
-  deriving (Eq, Show)
+  deriving (Eq, Show, Functor)
 
 empty :: OrderedMap k a
 empty = OrderedMap [] Map.empty
@@ -16,3 +21,6 @@ insert :: Ord k => k -> a -> OrderedMap k a -> Maybe (OrderedMap k a)
 insert k v (OrderedMap ks m)
   | Map.member k m = Nothing
   | otherwise      = Just $ OrderedMap (k : ks) $ Map.insert k v m
+
+toUnorderedMap :: OrderedMap k a -> Map.Map k a
+toUnorderedMap (OrderedMap _ m) = m
